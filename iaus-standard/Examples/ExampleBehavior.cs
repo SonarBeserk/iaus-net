ae17166141c0795f1b79a120cbbe458289ce13a2
SonarBeserk <sonarbeserk@gmail.com>
2023-11-16 21:54:50 -0500

    Create examples of behaviors and considerations

---
+++ iaus-standard/Examples/ExampleBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InfiniteAxisUtility.Examples
+{
+    /// <summary>
+    /// ExampleBehavior is an example of a behavior that prints to stdout
+    /// </summary>
+    public class ExampleBehavior: Behavior, IBehavior
+    {
+        public ExampleBehavior() : base("Example", Category.CategorySurvival)
+        {
+            // Consideration with a score of 0.25
+            Considerations.Add(new ExampleConsideration(1.0, 4.0));
+        }
+
+        public override void Run()
+        {
+            Console.WriteLine("Behavior Run");
+        }
+    }
+}
---
+++ iaus-standard/Examples/ExampleConsideration.cs
@@ -0,0 +1,24 @@
+﻿namespace InfiniteAxisUtility.Examples
+{
+    /// <summary>
+    /// ExampleConsideration is an example of a consideration
+    /// It takes two values `x` and `y` and normalizes the value by dividing `x` by `y`.
+    /// </summary>
+// ReSharper disable once UnusedType.Global
+    public class ExampleConsideration : Consideration, IConsideration
+    {
+        private readonly double _x;
+        private readonly double _y;
+
+        public ExampleConsideration(double x, double y) : base("Example", ResponseCurve.Linear)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override double Calculate()
+        {
+            return Normalize(_x/_y);
+        }
+    }
+}
