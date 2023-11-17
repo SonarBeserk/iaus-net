b959445fea119594481d89a2fdcef07926ff94a7
SonarBeserk <sonarbeserk@gmail.com>
2023-11-16 21:54:06 -0500

    Implement class for representing considerations

---
+++ iaus-standard/Consideration.cs
@@ -0,0 +1,80 @@
+﻿namespace InfiniteAxisUtility
+{
+	/// <summary>
+	/// The IConsideration interface represents a piece of data used to determine if a behavior should happen
+	/// </summary>
+	public interface IConsideration
+	{
+		/// <summary>
+		/// The name of the consideration (Hunger, Fatigue, ect)
+		/// </summary>
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// The mathematical equation used to model the data
+		/// </summary>
+		public ResponseCurve Curve
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Normalize calculates the score based on the curve type
+		/// </summary>
+		/// <returns>double between 0.0 and 1.0</returns>
+		public double Calculate();
+	}
+
+	/// <summary>
+	/// The IConsideration interface represents a piece of data used to determine if a behavior should happen
+	/// This class implements the IConsideration interface and provides a consistent structure to build on
+	/// </summary>
+	public abstract class Consideration: IConsideration
+	{
+		protected Consideration(string name, ResponseCurve curve)
+		{
+			Name = name;
+			Curve = curve;
+		}
+
+		public string Name { get; set; }
+		public ResponseCurve Curve { get; set; }
+
+		public abstract double Calculate();
+
+		/// <summary>
+		/// Calculate calls the response curve's method to compute the value
+		/// </summary>
+		/// <param name="x">The input value to pass into the equation</param>
+		/// <returns></returns>
+		protected double Normalize(double x)
+		{
+			return Curve.ComputeValue(x);
+		}
+	}
+}
+
+/*
+ * 		switch (Category) {
+			case Category.CategoryIdle:
+				score *= 1.0;
+				break;
+			case Category.CategorySurvival:
+				score *= 1.5;
+				break;
+			case Category.CategoryCombat:
+				score *= 2.0;
+				break;
+			case Category.CategoryEmergency:
+				score *= 5.0;
+				break;
+			case Category.CategoryUnknown:
+			default:
+				throw new InvalidEnumArgumentException("Category is unknown");
+		}
+ */
