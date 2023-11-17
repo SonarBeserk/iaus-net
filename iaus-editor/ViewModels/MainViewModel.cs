72bc91bc30633eb82203415f35974aba2c692022
SonarBeserk <sonarbeserk@gmail.com>
2023-11-17 18:01:23 -0500

    Hooked up basic file menu in Main editor view

--- iaus-editor/ViewModels/MainViewModel.cs
+++ iaus-editor/ViewModels/MainViewModel.cs
@@ -1,5 +1,15 @@
-﻿namespace InfiniteAxisUtility.Editor.ViewModels;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace InfiniteAxisUtility.Editor.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    public void ExitCommand()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+        {
+            lifetime.Shutdown();
+        }
+    }
 }
