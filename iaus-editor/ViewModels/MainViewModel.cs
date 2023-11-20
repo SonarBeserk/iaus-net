f28d3229a8c0b2fcdf6e783def7b4aa115412240
SonarBeserk <sonarbeserk@gmail.com>
2023-11-20 01:55:48 -0500

    Replace response curve tab contents with a dropdown bar with the curve type list

--- iaus-editor/ViewModels/MainViewModel.cs
+++ iaus-editor/ViewModels/MainViewModel.cs
@@ -1,10 +1,21 @@
-﻿using Avalonia;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 
 namespace InfiniteAxisUtility.Editor.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    public ObservableCollection<string> PresetCurves => new()
+    {
+        "Linear",
+        "Inverse Linear"
+    };
+
+    public int SelectedPresetCurve { get; set; }
+
     public void ExitCommand()
     {
         if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
