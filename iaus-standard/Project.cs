eeab8ee7a72dbd67e50dfae7a3c59f6549332a36
SonarBeserk <sonarbeserk@gmail.com>
2023-11-27 13:13:33 -0500

    Create concept of a project to organize behavior data

---
+++ iaus-standard/Project.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace InfiniteAxisUtility
+{
+    public class Project
+    {
+        public Project(string name = "Untitled")
+        {
+            Name = name;
+            Behaviors = new List<Behavior>();
+            BehaviorSets = new List<BehaviorSet>();
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public List<Behavior> Behaviors
+        {
+            get;
+            set;
+        }
+
+        public List<BehaviorSet> BehaviorSets
+        {
+            get;
+            set;
+        }
+    }
+}
