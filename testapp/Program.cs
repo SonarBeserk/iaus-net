22907e54c10f19f3fa5e98e11fb03b76821d452f
SonarBeserk <sonarbeserk@gmail.com>
2026-07-16 00:06:19 -0400

    Create test app to simplify testing

---
+++ testapp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Timers;
+using InfiniteAxisUtility;
+using InfiniteAxisUtility.Examples;
+using Timer = System.Timers.Timer;
+
+namespace testapp;
+
+public class Program
+{
+    private static Timer? _thinkTimer;
+    private static BehaviorProcessor? _processor;
+
+    static void Main(string[] args)
+    {
+        _processor = new BehaviorProcessor(new List<IBehavior>()
+        {
+            new ExampleBehavior()
+        });
+
+        _thinkTimer = new Timer(1000);
+        _thinkTimer.Elapsed += Think;
+        _thinkTimer.AutoReset = true;
+        _thinkTimer.Enabled = true;
+        Console.ReadLine();
+    }
+
+    private static void Think(object? sender, ElapsedEventArgs e)
+    {
+        if (_processor == null)
+        {
+            return;
+        }
+
+        var behavior = _processor.FindNextBehavior();
+        behavior.Run();
+    }
+}
